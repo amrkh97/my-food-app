8e3eebe2b011053621c9023f01efe4004de9f04d
Amr Khaled <amrkh97@gmail.com>
2020-10-13 14:54:50 +0200

    Fixed Navbar
    -Missing Dropdown

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-navigation',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
-
+  collapsed = true;
   constructor() { }
-
   ngOnInit(): void {
+
   }
 
 }
